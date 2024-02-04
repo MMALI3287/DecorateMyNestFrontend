c10cda3d1745bfaea97767cf163024110c85aff7
MD Musaddique Ali Erfan <erfanali3287@gmail.com>
2024-02-04 08:01:26 +0600

    added order items

--- src/components/pages/OrderItems.jsx
+++ src/components/pages/OrderItems.jsx
@@ -1,84 +1,188 @@
 // import React from 'react';
 
-import { useForm } from "react-hook-form";
+import { useForm, Controller, useWatch } from "react-hook-form";
 import FormInput from "../atoms/FormInput/FormInput";
+import ApiCalls from "../../apis/APICalls";
+import { useEffect, useState } from "react";
 
 const OrderItems = () => {
-    const {
-        handleSubmit,
-        control,
-        formState: { errors },
-        // reset,
-    } = useForm({
-        mode: "onChange",
-    });
-    return (
-        <div className="font-sans">
-            <h1 className='text-3xl w-96 font-bold text-white bg-gradient-to-b from-blue-900 to-black p-3 my-10 text-center mx-auto rounded-xl shadow-2xl'>
-            Order Items</h1>
-            <form onSubmit={handleSubmit()} className="w-1/2 mx-auto">
-                <select className="select select-bordered mx-auto my-auto block w-full p-2 text-black font-semibold bg-gray-200 border-none mt-7  overflow-x-scroll max-w-screen">
-                    <option className="text-black" disabled selected>Material Name</option>
-                    <option className="text-black">Han Solo</option>
-                    <option className="text-black">Greedo</option>
-                </select>
-                <select className="select select-bordered mx-auto my-auto block w-full p-2 text-black font-semibold bg-gray-200 border-none mt-7  overflow-x-auto max-w-full">
-                    <option className="text-black" disabled selected>Material Name</option>
-                    <option className="text-black">Han Solo</option>
-                    <option className="text-black">Greedo</option>
-                </select>
-                <FormInput
-                    className="border-2"
-                    labelText="Quantity"
-                    type="number"
-                    name="name"
-                    defaultValue={""}
-                    control={control}
-                    errors={errors}
-                    rules={{
-                        required: "Quantity is required",
-                        maxLength: {
-                            value: 20,
-                            message: "Quantity should be less than 20 letters",
-                        },
-                        minLength: {
-                            value: 6,
-                            message: "Quantity should be more than 6 letters",
-                        },
-                    }}
-                />
-                <FormInput
-                    className="border-2"
-                    labelText="Price"
-                    type="number"
-                    name="number"
-                    defaultValue={""}
-                    control={control}
-                    errors={errors}
-                    rules={{
-                        required: "Price is required",
-                        maxLength: {
-                            value: 20,
-                            message: "Price should be less than 20 letters",
-                        },
-                        minLength: {
-                            value: 6,
-                            message: "Price should be more than 6 letters",
-                        },
-                    }}
-                />
-                <label className="form-control">
-                    <div className="label">
-                        <span className="label-text mx-auto my-auto block w-full font-bold text-blue-900">Description</span>
+  const [materials, setMaterials] = useState([]);
+  const [success, setSuccess] = useState(false);
+  const [vendors, setVendors] = useState([]);
+  const [vendorId, setVendorId] = useState([]);
+  const api = new ApiCalls();
 
-                    </div>
-                    <textarea className="mx-auto my-auto block w-full p-2 textarea textarea-bordered h-24" placeholder="Description"></textarea>
+  useEffect(() => {
+    async function fetchData() {
+      const materialItems = await api.getMaterialInventories();
+      const authenticatedAccounts = await api.getAuthenticaions();
+      const vendorIds = await api.getVendors();
 
-                </label>
+      const vendorsWithRole = authenticatedAccounts.filter(
+        (account) => account.Role?.toLowerCase() === "vendor"
+      );
 
-            </form>
-        </div>
-    );
+      const vendors = vendorsWithRole.map((vendor) => {
+        const vendorDetails = vendorIds.find(
+          (vendorId) => vendorId.AuthId === vendor.AuthId
+        );
+        return {
+          ...vendor,
+          ...vendorDetails,
+        };
+      });
+
+      setVendors(vendors);
+      setMaterials(materialItems);
+      setVendorId(vendorIds);
+      console.log(vendors);
+    }
+    fetchData();
+  }, []);
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    setValue,
+    // reset,
+  } = useForm({
+    mode: "onChange",
+  });
+
+  const materialId = useWatch({
+    control,
+    name: "MaterialId",
+    defaultValue: "",
+  });
+
+  const onSubmit = (data) => {
+    try {
+      data["TransactionDate"] = new Date();
+      const financialTrans = api.createFinancialTransaction(data);
+      data["TransactionId"] = financialTrans.TransactionId;
+      const materialTrans = api.createMaterialTransaction(data);
+      data["TransactionId"] = materialTrans.TransactionId;
+      const order = api.createOrder(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="font-sans">
+      <h1 className="text-3xl w-96 font-bold text-white bg-gradient-to-b from-blue-900 to-black p-3 my-10 text-center mx-auto rounded-xl shadow-2xl">
+        Order Items
+      </h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="w-1/2 mx-auto">
+        <Controller
+          name="MaterialId"
+          control={control}
+          defaultValue=""
+          rules={{ required: "Material Name is required" }}
+          render={({ field }) => (
+            <div>
+              <h6 className="text-sm font-bold text-blue-950 mb-1">
+                Material Name
+              </h6>
+              <select
+                {...field}
+                className="select select-bordered mx-auto my-auto block w-full p-2 text-black font-semibold bg-gray-200"
+              >
+                <option className="text-black" disabled value="">
+                  Choose an option
+                </option>
+                {materials.map((materials) => (
+                  <option
+                    className="text-black"
+                    key={materials.MaterialId}
+                    value={materials.MaterialId}
+                  >
+                    {materials.Name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          )}
+        />
+        <Controller
+          name="VendorId"
+          control={control}
+          defaultValue=""
+          rules={{ required: "Vendor is required" }}
+          render={({ field }) => (
+            <div>
+              <h6 className="text-sm font-bold text-blue-950 mb-1">Vendor</h6>
+              <select
+                {...field}
+                className="select select-bordered mx-auto my-auto block w-full p-2 text-black font-semibold bg-gray-200"
+              >
+                <option className="text-black" disabled value="">
+                  Choose an option
+                </option>
+                {vendors.map((vendor) => (
+                  <option
+                    className="text-black"
+                    key={vendor.VendorId}
+                    value={vendor.VendorId}
+                  >
+                    {vendor.UserName}, ({vendor.CompanyName})
+                  </option>
+                ))}
+              </select>
+            </div>
+          )}
+        />
+        <FormInput
+          className="border-2"
+          labelText="Quantity"
+          type="number"
+          name="Quantity"
+          defaultValue={""}
+          control={control}
+          errors={errors}
+          rules={{
+            required: "Quantity is required",
+          }}
+        />
+        <FormInput
+          className="border-2"
+          labelText="Price"
+          type="number"
+          name="Amount"
+          defaultValue={""}
+          control={control}
+          errors={errors}
+          rules={{
+            required: "Price is required",
+          }}
+        />
+        <Controller
+          name="Description"
+          control={control}
+          defaultValue=""
+          rules={{ required: "Description is required" }}
+          render={({ field }) => (
+            <label className="form-control">
+              <div className="label">
+                <span className="label-text mx-auto my-auto block w-full font-bold text-blue-900">
+                  Description
+                </span>
+              </div>
+              <textarea
+                {...field}
+                className="mx-auto my-auto block w-full p-2 textarea textarea-bordered h-24"
+                placeholder="Catalog Description"
+              />
+            </label>
+          )}
+        />
+        <button type="submit" className="text-xl btn btn-primary w-full mt-5">
+          Order Item
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default OrderItems;
